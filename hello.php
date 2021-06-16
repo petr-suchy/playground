7f474fe480bd8933852743549d13b7e3926b72f2
Petr Suchý <petrs.sokolov@seznam.cz>
2021-06-16 22:34:02 +0200

    Clean up

--- hello.php
+++ hello.php
@@ -1,33 +1,4 @@
 <?php
 
-$value = 123;
+echo "Hello, World!\n";
 
-echo "test({$value}):" . test($value) . "\n";
-echo "test100({$value}):" . test100($value) . "\n";
-echo "test200({$value}):" . test200($value) . "\n";
-echo "test500({$value}):" . test500($value) . "\n";
-
-function test($value)
-{
-    return $value;
-}
-
-function test100($value)
-{
-    return 100 + $value;
-}
-
-function test200($value)
-{
-    return 200 + $value;
-}
-
-function test400($value)
-{
-    return 400 + $value;
-}
-
-function test500($value)
-{
-    return 500 + $value;
-}
