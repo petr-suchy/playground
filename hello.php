17db53d0e259429a92dfb1b19249ea4296ecbba4
Petr Suchý <petrs.sokolov@seznam.cz>
2021-06-16 21:26:41 +0200

    Add function test100()

--- hello.php
+++ hello.php
@@ -3,8 +3,14 @@
 $value = 123;
 
 echo "test({$value}):" . test($value) . "\n";
+echo "test100({$value}):" . test100($value) . "\n";
 
 function test($value)
 {
     return $value;
+}
+
+function test100($value)
+{
+    return 100 + $value;
 }
